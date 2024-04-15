4f7527a3c8a1bedc59816091d6d5151b6a436305
Tashrif2001 <tashrifpro@gmail.com>
2024-04-15 19:47:17 -0400

    done except news page(api premium)

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,6 +6,7 @@ import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
 import LineChart from './LineChart';
+import Loader from './Loader';
 
 const {Title, Text} = Typography;
 const { Option } = Select;
@@ -17,7 +18,7 @@ const CryptoDetails = () => {
   const { data: coinHistory } = useGetCryptoHistoryQuery({coinId, timePeriod});
   const cryptoDetails = data?.data?.coin;
   
-  if(isFetching) return 'Loading...';
+  if(isFetching) return <Loader />;
 
   const time = ['3h', '24h', '7d', '30d', '1y','3m', '3y', '5y'];
 
@@ -92,12 +93,14 @@ const CryptoDetails = () => {
                 ))}
             </Col>
         </Col>
-        <Col className='coin-desc-link'>
-          <Row classname='coin-desc'>
+        <Col className='coin-desc-link' style={{justifyContent: 'space-between'}}>
+          <Row classname='coin-desc' style={{display: 'flex', flexDirection: 'column'}}>
                 <Title level={3} className='coin-details-heading'>
                   What is {cryptoDetails.name}?
                 </Title>
+                <div>
                 {HTMLReactParser(cryptoDetails.description)}
+                </div>
           </Row>
           <Col className='coin-links'>
             <Title level={3} className='coin-details-heading'>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Loader from './Loader';
 
 
 const { Text, Title } = Typography;
@@ -14,7 +15,7 @@ const News = ({simplified}) => {
   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
   const { data } = useGetCryptosQuery(100);
   
-  if(!cryptoNews?.value) return 'Loading...';
+  if(!cryptoNews?.value) return <Loader />;
 
   return (
     <Row gutter={[24,24]}>
